95d7271f677252a77e4148af3e13cb400caf36a1
Ryan Paulson <ryanpdesigns@gmail.com>
2025-05-05 12:40:11 -0500

    refactored search filter components

--- frontend/app/components/search-filter.jsx
+++ frontend/app/components/search-filter.jsx
@@ -1,26 +1,28 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
+import SearchFilterNumbox from './search-filter-numbox';
 import SearchFilterCheckbox from './search-filter-checkbox';
-import CategoryItem from './category-item';
 
 const SearchFilter = ({
 	searchParams,
-	searchResults,
+	searchResults /*Array of Objects */,
 	searchCategories,
-	set,
+	setFilteredResults,
 }) => {
 	const bedOptions = [1, 2, 3, 4, 5];
 	const bathOptions = [1, 2, 3, 4, 5];
 	const floors = [1, 2];
 
 	const { minsqft, maxsqft, beds, baths } = searchParams;
+	const checkboxRef = useRef([]);
 
 	const handleStyleClick = (item) => {
-		console.log('item', item);
+		// console.log('item', item);
+		console.log('checked?', checkboxRef.current.checked);
 		const results = searchResults.filter(
 			(e) => e.categoryName?.toString() == item?.toString()
 		);
-		setFilteredResults(results);
+		// setFilteredResults(results);
 	};
 
 	return (
@@ -31,7 +33,7 @@ const SearchFilter = ({
 				<h2>Beds</h2>
 				<div className="flex gap-2">
 					{bedOptions.map((option, index) => (
-						<SearchFilterCheckbox
+						<SearchFilterNumbox
 							name="beds"
 							option={option}
 							paramChecked={option.toString() == beds?.toString()}
@@ -42,7 +44,7 @@ const SearchFilter = ({
 				<h2>Baths</h2>
 				<div className="flex gap-2">
 					{bathOptions.map((option, index) => (
-						<SearchFilterCheckbox
+						<SearchFilterNumbox
 							name="baths"
 							option={option}
 							paramChecked={
@@ -57,22 +59,31 @@ const SearchFilter = ({
 			<div className="p-2">
 				<h2>Styles</h2>
 				<div className="flex flex-col">
-					{searchCategories?.map((item, index) => (
-						<div
-							className="flex"
-							key={`${item}-${index}`}
-							onClick={() => handleStyleClick(item)}>
-							<input
-								type="checkbox"
-								id={item}
-								className="peer accent-teal-600"
-							/>
-							<label
-								htmlFor={item}
-								className="select-none py-1 px-4 text-black">
-								{item}
-							</label>
-						</div>
+					{searchCategories?.map((option, index) => (
+						// <div
+						// 	className="flex"
+						// 	key={`${item}-${index}`}
+						// 	onClick={() => handleStyleClick(item)}>
+						// 	<input
+						// 		type="checkbox"
+						// 		id={item}
+						// 		className="peer accent-teal-600"
+						// 		ref={checkboxRef}
+						// 	/>
+						// 	<label
+						// 		htmlFor={item}
+						// 		className="select-none py-1 px-4 text-black">
+						// 		{item}
+						// 	</label>
+						// </div>
+						<SearchFilterCheckbox
+							name="baths"
+							option={option}
+							paramChecked={
+								option.toString() == baths?.toString()
+							}
+							key={`${option}-${index}`}
+						/>
 					))}
 				</div>
 				<hr className="text-gray-300" />
